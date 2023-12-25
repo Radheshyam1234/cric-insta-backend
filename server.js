4c90a76acdabcfc92cc596a856ef63a06adcf762
Radheshyam1234 <rdk7352669258@gmail.com>
2023-12-25 13:32:17 +0530

    added basic-setup and db-connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { initializeConnection } = require("./database/db-connection");
+
+app.use(express.json());
+app.use(cors());
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 8080;
+initializeConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello from cric-insta backend");
+});
+
+const server = app.listen(process.env.PORT || PORT, () => {
+  console.log(`listening to port 8080`);
+});
