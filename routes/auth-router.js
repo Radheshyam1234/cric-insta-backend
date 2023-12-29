f4c5537ca88d8ff449c6d25fc6e99ddcdfdba9cb
Radheshyam1234 <88738458+Radheshyam1234@users.noreply.github.com>
2023-12-30 01:42:51 +0530

    handled reset-password

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -5,10 +5,12 @@ const {
   signInUser,
   sendOTP,
   verifyOtp,
+  resetPassword,
 } = require("../controllers/auth-controller/auth-controller");
 
 router.route("/login").post(signInUser);
 router.route("/sendotp").post(sendOTP);
 router.route("/verifyotp").post(verifyOtp);
+router.route("/reset-password").post(resetPassword);
 
 module.exports = router;
