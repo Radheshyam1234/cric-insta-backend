79ef0f864e3a836b56f1e22ffe8a26e887d0b521
Radheshyam1234 <rdk7352669258@gmail.com>
2024-01-06 01:42:27 +0530

    handled account already-exist

--- controllers/auth-controller/auth-controller.js
+++ controllers/auth-controller/auth-controller.js
@@ -19,6 +19,16 @@ const sendOTP = async (req, res) => {
   if (!emailPattern.test(email))
     return res.status(401).json({ errorText: "Enter a valid email" });
 
+  console.log(requestType);
+  if (requestType === "create user") {
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+      return res.status(403).json({
+        message: "Account already exists for this email",
+      });
+    }
+  }
+
   const transporter = createNodemailTransporter();
   const OTP = generateOtp();
 
